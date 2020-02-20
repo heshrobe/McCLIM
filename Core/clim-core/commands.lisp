6d803b7ca0546bb562c67d170a861c71587857f5
hes <hes@csail.mit.edu>
2020-02-20 18:18:12 -0500

    this fixes #967 this fixes #955
    In file clim-core/commands.lisp
    
    Add an :initform nil for the docuemtation field of %menu-item.  This
    is necessary to avoid an error if the mouse moves over a command menu
    item.  The problem is that if a command-menu-item takes arguments, when
    the mouse moves over the item an error will signaled rather than
    querying for the rest of the arguments; this is because the
    documentation was not being initialized.
    
    In file clim-core/gadgets/menu.lisp
    
    Introduce a new presentation type command-menu-item to be used in command menus diplayed in appllication-frames.
    Add a translator from the command-menu-item to command.
    Use the new presentation-type when displaying command table menu
    
    In the ACCEPT presentation-method for COMMAND:
    
    1. Use WITH-INPUT-CONTEXT for presentation-type 'command so that this method
        can handle mouse clicks on objects that translate to (or are presented as) commands
    
    2. In the handler for WITH-INPUT-CONTEXT, check if the command is a partial command and
        if so call the partial command parser and note that the input needs to be replaced
    
    3. If the input needs to be replaced, then explictly provide the :RESCAN NIL option to
        PRESENTATION-REPLACE-INPUT.
        This has a different effect than not providing :rescan at all (which seems to default to T).
    
    The reason for 3 is that the command returned by clicking on an object may contain arguments
     that print (or present) unreadably. PRESENTATION-REPLACE-INPUT presents that command into the input
     buffer and if :RESCAN NIL is not provided, then ACCEPT-1 will call the ACCEPT presentation method,
     invoking the command-parser again.  However, this time the input buffer contains the unreadable string
     for the argument in the command and the command argument parser will then signal an error.
     This was observed in Clouseau.
    
    The reason for 2 is that a command-menu in an application might contain a command with missing arguments.
     Without this fix, clicking on the item will cause and error message saying that the command is incomplete.

--- Core/clim-core/commands.lisp
+++ Core/clim-core/commands.lisp
@@ -49,7 +49,7 @@
   ((menu-name :reader command-menu-item-name :initarg :menu-name)
    (type :initarg :type :reader command-menu-item-type)
    (value :initarg :value :reader command-menu-item-value)
-   (documentation :initarg :documentation)
+   (documentation :initarg :documentation :initform nil)
    (text-style :initarg :text-style :initform nil)
    (keystroke :initarg :keystroke)))
 
@@ -1300,14 +1300,36 @@ examine the type of the command menu item to see if it is
 (define-presentation-method accept ((type command) stream
 				    (view textual-view)
 				    &key)
-  (let ((command (funcall *command-parser* command-table stream)))
-    (cond ((null command)
-	   (simple-parse-error "Empty command"))
-          ((partial-command-p command)
-           (funcall *partial-command-parser*
-            command-table stream command
-            (position *unsupplied-argument-marker* command)))
-	  (t (values command type)))))
+  (setq command-table (find-command-table command-table))
+  (let ((start-position (and (input-editing-stream-p stream)
+                             (stream-scan-pointer stream)))
+        (replace-input-p nil))
+    (multiple-value-bind (object new-type)
+        ;; We establish a new input context so that clicks on command-menu-items
+        ;; throw to us. This will let's us handle "partial commands" below.
+        ;; This only works for partial commands coming by clicking on
+        ;; application-frame menus.  Clicking on a presentation of something as a
+        ;; command with unsupplied arguments with instead by caught by accept-1
+        ;; and bypass this handling.
+        (with-input-context ('command :override nil)
+	    (object presentation-type event options)
+            (funcall *command-parser* command-table stream)
+	  (t (when (getf options :echo t)
+	       (setq replace-input-p t))
+	     (values object presentation-type)))
+      (declare (ignore new-type))
+      (cond ((partial-command-p object)
+             (values (funcall *partial-command-parser*
+                              command-table stream object start-position)
+                     type))
+            (t (when replace-input-p
+                  (presentation-replace-input stream object type view
+                                              :buffer-start start-position
+                                              ;;; This is important in case the clicked
+                                              ;;; on thing returned something unparseable
+                                              :rescan nil
+                                              ))
+               (values object type))))))
 
 ;;; A presentation type for empty input at the command line; something for
 ;;; read-command to supply as a default.  The command is defined in
